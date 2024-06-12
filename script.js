784b5967708214371c4d5099ec3ec208c436bc5e
Teoramites <teoramites@gmail.com>
2024-06-11 23:53:46 -0300

    seguimos con pruebas

--- script.js
+++ script.js
@@ -16,25 +16,24 @@ document.getElementById('carnetForm').addEventListener('submit', function(event)
     // Obtener la escala de la densidad de píxeles
     const scale = window.devicePixelRatio;
 
-    // Obtener las dimensiones originales
-    const carnetContainer = document.getElementById('carnetContainer');
-    const originalWidth = carnetContainer.offsetWidth;
-    const originalHeight = carnetContainer.offsetHeight;
-
     // Ajustar el tamaño del contenedor del carnet
-    carnetContainer.style.width = `${originalWidth * scale}px`;
-    carnetContainer.style.height = `${originalHeight * scale}px`;
+    const carnetElement = document.getElementById('carnet');
+    const originalWidth = carnetElement.offsetWidth;
+    const originalHeight = carnetElement.offsetHeight;
+
+    carnetElement.style.width = `${originalWidth * scale}px`;
+    carnetElement.style.height = `${originalHeight * scale}px`;
 
     // Generar y descargar la imagen del carnet
-    html2canvas(carnetContainer, { scale }).then(canvas => {
+    html2canvas(carnetElement, { scale }).then(canvas => {
         const link = document.createElement('a');
         link.href = canvas.toDataURL('image/png');
         link.download = 'carnet.png';
         link.click();
 
         // Restaurar el tamaño original del contenedor del carnet
-        carnetContainer.style.width = `${originalWidth}px`;
-        carnetContainer.style.height = `${originalHeight}px`;
+        carnetElement.style.width = `${originalWidth}px`;
+        carnetElement.style.height = `${originalHeight}px`;
     });
 });
 
